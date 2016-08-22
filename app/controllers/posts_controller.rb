61f3f0020464c78ed74166e4a5b8112164fa9896
John Lindner <jlogin56@gmail.com>
2016-08-21 22:43:29 -0500

    Add feature to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
 	def index
 		@all_posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		Post.create(params.require(:post).permit(:title, :body))
+		redirect_to '/posts'
+	end
 end
